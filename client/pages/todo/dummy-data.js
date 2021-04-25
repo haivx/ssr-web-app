20703af1a4ce4949899f317e32c19dd56725361b
Xuan Hai <haivx0206@gmail.com>
2021-04-25 23:23:13 +0700

    refactor example: optimize re render by usereducer-usecallback-memo

---
+++ client/pages/todo/dummy-data.js
@@ -0,0 +1,32 @@
+export const dummy = [
+    {
+        id: 1,
+        name: 'Wake up on time',
+        time: new Date(),
+        checked: false,
+    },
+    {
+        id: 2,
+        name: 'Go to work',
+        time: new Date(),
+        checked: true,
+    },
+    {
+        id: 3,
+        name: 'Go to the gym',
+        time: new Date(),
+        checked: true,
+    },
+    {
+        id: 4,
+        name: 'Play video games',
+        time: new Date(),
+        checked: false,
+    },
+    {
+        id: 5,
+        name: 'Go to coffee shop',
+        time: new Date(),
+        checked: false,
+    },
+]
